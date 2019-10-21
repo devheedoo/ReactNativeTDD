4fafab9650dcf8cbaabc72bb0b3ea5c6da2c676f
devheedoo <devheedoo@gmail.com>
2019-10-22 07:30:28 +0900

    Second make it green

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {Platform, Text, View} from 'react-native';
+import AddToDo from './AddTodo';
 
 export default class App extends Component {
   render() {
     return (
       <View testID="welcome">
         <Text>Todo TDD</Text>
+        <AddToDo></AddToDo>
       </View>
     );
   }
