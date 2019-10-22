87f87d67c3b75e5441395c3b2feea8598ee5ca8a
Heedo Kim <devheedoo@gmail.com>
2019-10-22 16:02:57 +0900

    End

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -7,24 +7,24 @@ describe('Example', () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
-  // it('should add ToDoItem', async () => {
-  //   const text = 'a new ToDo Item';
-  //   await element(by.id('textInput')).tap();
-  //   await element(by.id('textInput')).typeText(text);
-  //   await element(by.id('addButton')).tap();
-  //   await expect(element(by.text(text).withAncestor(by.id('ToDoList')))).toBeVisible();
-  // });
-
-  // it('should complete ToDoItem', async () => {
-  //   const text = 'a new ToDo Item';
-  //   await element(by.id('textInput')).tap();
-  //   await element(by.id('textInput')).typeText(text);
-  //   await element(by.id('addButton')).tap();
-  //   await element(by.id('completeButton')).multiTap(2);
-  //   await expect(element(by.id('completed').and(by.text(text)).withAncestor(by.id('ToDoList')))).toBeVisible();
-  // });
+  it('should add ToDoItem', async () => {
+    const text = 'a new ToDo Item';
+    await element(by.id('textInput')).tap();
+    await element(by.id('textInput')).typeText(text);
+    await element(by.id('addButton')).tap();
+    await expect(element(by.text(text).withAncestor(by.id('ToDoList')))).toBeVisible();
+  });
 
   it('should complete ToDoItem', async () => {
+    const text = 'a new ToDo Item';
+    await element(by.id('textInput')).tap();
+    await element(by.id('textInput')).typeText(text);
+    await element(by.id('addButton')).tap();
+    await element(by.id('completeButton')).multiTap(2);
+    await expect(element(by.id('completed').and(by.text(text)).withAncestor(by.id('ToDoList')))).toBeVisible();
+  });
+
+  it('should delete ToDoItem', async () => {
     const text = 'a new ToDo Item';
     await element(by.id('textInput')).tap();
     await element(by.id('textInput')).typeText(text);
--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ export default class App extends Component {
 
   onCompleted = (index) => {
     this.setState({
-      items: [
-        ...this.state.items.slice(0, index),
-        ...this.state.items.slice(index + 1)
-      ]
+      items: this.state.items.map((item, i) => {
+        if (index !== i) return item;
+        return { ...item, completed: !item.completed }
+      })
     });
   }
 
   onDeleted = (index) => {
     this.setState({
-      items: this.state.items.map((item, i) => {
-        if (index !== i) return item;
-        return;
-      })
+      items: [
+        ...this.state.items.slice(0, index),
+        ...this.state.items.slice(index + 1)
+      ]
     });
   }
 
