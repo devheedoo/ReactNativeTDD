03a47391e8a134aff3e6591fd2fdfe0a82c8459b
Heedo Kim <devheedoo@gmail.com>
2019-10-22 14:14:36 +0900

    Green: ToDoItem interaction

--- __tests__/ToDoItem-test.js
+++ __tests__/ToDoItem-test.js
@@ -50,24 +50,49 @@ describe('Interaction', () => {
   let wrapper;
   let props;
 
-  beforeEach(() => {
-    props = {
-      item: {
-        text: 'first ToDo',
-        completed: false
-      },
-      index: 0,
-      onCompleted: jest.fn()
-    }
-
-    wrapper = shallow(<ToDoItem {...props}></ToDoItem>);
-
-    wrapper.find('Button').at(0).prop('onPress')();
-    wrapper.find('Button').at(1).prop('onPress')();
+  describe('Complete feature', () => {
+    beforeEach(() => {
+      props = {
+        item: {
+          text: 'first ToDo',
+          completed: false
+        },
+        index: 0,
+        onCompleted: jest.fn(),
+        onDeleted: jest.fn(),
+      }
+  
+      wrapper = shallow(<ToDoItem {...props}></ToDoItem>);
+  
+      wrapper.find('Button').at(0).prop('onPress')();
+    });
+  
+    it('should call onCompleted callback with index', () => {
+      expect(props.onCompleted).toHaveBeenCalledTimes(1);
+      expect(props.onCompleted).toHaveBeenCalledWith(props.index);
+    });
   });
 
-  it('should call onAdded callback with input text', () => {
-    expect(props.onCompleted).toHaveBeenCalledTimes(1);
-    expect(props.onCompleted).toHaveBeenCalledWith(props.index);
+  describe('Complete feature', () => {
+    beforeEach(() => {
+      props = {
+        item: {
+          text: 'first ToDo',
+          completed: false
+        },
+        index: 0,
+        onCompleted: jest.fn(),
+        onDeleted: jest.fn(),
+      }
+  
+      wrapper = shallow(<ToDoItem {...props}></ToDoItem>);
+  
+      wrapper.find('Button').at(1).prop('onPress')();
+    });
+    
+    it('should call onDeleted callback with index', () => {
+      expect(props.onDeleted).toHaveBeenCalledTimes(1);
+      expect(props.onDeleted).toHaveBeenCalledWith(props.index);
+    });
   });
 })
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default class ToDoItem extends Component {
+  handleCompletedPress = () => {
+    const { onCompleted, index } = this.props;
+    onCompleted(index);
+  }
+
+  handleDeletedPress = () => {
+    const { onDeleted, index } = this.props;
+    onDeleted(index);
+  }
+  
   render() {
     const { item } = this.props;
     return (
       <View style={item.completed ? styles.completed : styles.default}>
         <Text></Text>
-        <Button title="C"></Button>
-        <Button title="D"></Button>
+        <Button title="C" onPress={this.handleCompletedPress}></Button>
+        <Button title="D" onPress={this.handleDeletedPress}></Button>
       </View>
     );
   }
